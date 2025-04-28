54a9e33be95e7b8730fdf58a3cb6c7216ffea3d3
Shoots <31416585+BigShoots@users.noreply.github.com>
2025-04-27 20:08:28 -0500

    Add Mattel Store

---
+++ src/store/model/mattel.ts
@@ -0,0 +1,102 @@
+import {Store} from './store';
+
+export const Mattel: Store = {
+  currency: '$',
+  labels: {
+    inStock: {
+      container: 'button.product__add-to-cart',
+      text: ['Add to Bag'],
+    },
+    outOfStock: {
+      container: 'button.product__add-to-cart',
+      text: ['Sold Out'],
+    },
+  },
+  links: [
+    {
+      brand: 'Hot Wheels',
+      model: 'Lewis Hamilton',
+      series: 'Mercedes-AMG Petronas F1',
+      url: 'https://creations.mattel.com/products/mercedes-amg-petronas-formula-one-team-lewis-hamilton-hnf24',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'George Russell',
+      series: 'Mercedes-AMG Petronas F1',
+      url: 'https://creations.mattel.com/products/mercedes-amg-petronas-formula-one-team-george-russell-hnf30',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Max Verstappen',
+      series: 'Oracle Red Bull Racing',
+      url: 'https://creations.mattel.com/products/oracle-red-bull-racing-max-verstappen-hnf26',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Sergio Perez',
+      series: 'Oracle Red Bull Racing',
+      url: 'https://creations.mattel.com/products/oracle-red-bull-racing-sergio-perez-hnf31',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Charles Leclerc',
+      series: 'Scuderia Ferrari',
+      url: 'https://creations.mattel.com/products/scuderia-ferrari-charles-leclerc-hnf23',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Carlos Sainz',
+      series: 'Scuderia Ferrari',
+      url: 'https://creations.mattel.com/products/scuderia-ferrari-carlos-sainz-hnf32',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Lando Norris',
+      series: 'McLaren F1 Team',
+      url: 'https://creations.mattel.com/products/mclaren-formula-1-team-lando-norris-hnf27',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Oscar Piastri',
+      series: 'McLaren F1 Team',
+      url: 'https://creations.mattel.com/products/mclaren-formula-1-team-oscar-piastri-hnf33',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Fernando Alonso',
+      series: 'Aston Martin Aramco F1 Team',
+      url: 'https://creations.mattel.com/products/aston-martin-aramco-formula-one-team-fernando-alonso-hnf25',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Lance Stroll',
+      series: 'Aston Martin Aramco F1 Team',
+      url: 'https://creations.mattel.com/products/aston-martin-aramco-formula-one-team-lance-stroll-hnf34',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Valtteri Bottas',
+      series: 'Stake F1 Team Kick Sauber',
+      url: 'https://creations.mattel.com/products/stake-f1-team-kick-sauber-valtteri-bottas-hnf29',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Zhou Guanyu',
+      series: 'Stake F1 Team Kick Sauber',
+      url: 'https://creations.mattel.com/products/stake-f1-team-kick-sauber-zhou-guanyu-hnf35',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Alexander Albon',
+      series: 'Williams Racing',
+      url: 'https://creations.mattel.com/products/williams-racing-alexander-albon-hnf28',
+    },
+    {
+      brand: 'Hot Wheels',
+      model: 'Logan Sargeant',
+      series: 'Williams Racing',
+      url: 'https://creations.mattel.com/products/williams-racing-logan-sargeant-hnf36',
+    },
+  ],
+  name: 'mattel',
+};
